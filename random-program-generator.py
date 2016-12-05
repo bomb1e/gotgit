bdc3a6be376cf153694f59decd8c0c46f125194a
Eric Githinji <ericgithinjimaina@gmail.com>
2016-12-05 07:40:32 +0000

    Initial Commit

---
+++ random-program-generator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import random
+import uuid
+
+from pgen import pgen_opts, ProgGenerator
+from pygen.cgen import CodeGenerator
+
+unique = str(uuid.uuid4()) + '.py'
+
+if __name__ == "__main__":
+    pgen = ProgGenerator(pgen_opts, random.Random())
+
+    m = pgen.generate()
+
+    cgen = CodeGenerator()
+
+    output = cgen.generate(m)
+
+file = open(unique, 'w')
+file.write(output)
+file.close()
+print(output) 
